1de6f0d2fa7d6a09dc7120e1feb8fafd146555cb
bug41 <bugkingus@gmail.com>
2024-04-22 22:40:38 +0900

    나머지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage"
 import SinglePage from "./routes/singlePage/singlePage"
 import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import ProfilePage from "./routes/profilePage/profilePage";
 
 function App() {
 
@@ -22,6 +23,10 @@ function App() {
         {
           path:"/:id",
           element:<SinglePage/>
+        },
+        {
+          path:"/profile",
+          element:<ProfilePage/>
         },
       ]
     },
